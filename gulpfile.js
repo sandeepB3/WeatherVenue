e07b93720415d6e98bb2ffd2419bacbef080ad97
bacloud14 <bacloud14@gmail.com>
2021-02-26 16:59:33 +0100

    gulp compress task

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const terser = require('gulp-terser')
+const pipeline = require('readable-stream').pipeline
+
+gulp.task('compress', function () {
+  return pipeline(
+    gulp.src(['src/js/js_variables.js', 'src/js/lang_mappings.js', 'src/js/html_holders.js', 'src/js/accessibility.js', 'src/js/GMap.js']),
+    sourcemaps.init(),
+    concat('all.js'),
+    terser(),
+    sourcemaps.write('.'),
+    // uglify(),
+    gulp.dest('public/js/')
+  )
+})

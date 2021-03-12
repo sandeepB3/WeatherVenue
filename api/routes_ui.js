7f4735df1e2873b5ad9523bbec3e1c0097bb60aa
bacloud14 <bacloud14@gmail.com>
2021-03-12 22:30:22 +0100

    localise tour guide

--- api/routes_ui.js
+++ api/routes_ui.js
@@ -8,19 +8,45 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 routerUI.get('/', function rootHandler (req, res) {
-  res.render('index', { key: process.env.GOOGLE_MAPS_API_KEY, pass: pass, scripts: helpers.mappings0, dependencies: helpers.mappings })
+  res.render('index', {
+    key: process.env.GOOGLE_MAPS_API_KEY,
+    pass: pass,
+    scripts: helpers.mappings0,
+    dependencies: helpers.mappings,
+    messages: helpers.messages,
+    lang: 'en'
+  })
 })
 
 routerUI.get('/ar', function rootHandler (req, res) {
-  res.render('index_ar', { key: process.env.GOOGLE_MAPS_API_KEY, pass: pass, scripts: helpers.mappings0, dependencies: helpers.mappings })
+  res.render('index_ar', {
+    key: process.env.GOOGLE_MAPS_API_KEY,
+    pass: pass,
+    scripts: helpers.mappings0,
+    dependencies: helpers.mappings,
+    messages: helpers.messages,
+    lang: 'ar'
+  })
 })
 
 routerUI.get('/fr', function rootHandler (req, res) {
-  res.render('index_fr', { key: process.env.GOOGLE_MAPS_API_KEY, pass: pass, scripts: helpers.mappings0, dependencies: helpers.mappings })
+  res.render('index_fr', {
+    key: process.env.GOOGLE_MAPS_API_KEY,
+    pass: pass,
+    scripts: helpers.mappings0,
+    dependencies: helpers.mappings,
+    messages: helpers.messages,
+    lang: 'fr'
+  })
 })
 
-routerUI.get('/weather_map_view', function rootHandler (req, res) {
-  res.render('weather_map_view', { key: process.env.GOOGLE_MAPS_API_KEY, pass: pass, scripts: helpers.mappings0, dependencies: helpers.mappings })
+routerUI.get('/weather_map_view', function rootHandler(req, res) {
+  res.render('weather_map_view', {
+    key: process.env.GOOGLE_MAPS_API_KEY,
+    pass: pass,
+    scripts: helpers.mappings0,
+    dependencies: helpers.mappings
+  })
 })
 
 module.exports = routerUI
--- helpers.js
+++ helpers.js
@@ -88,7 +88,7 @@ function formatCities (cities, weathers, pollutions) {
 }
 
 const messages = {
-  notFoundMessages : {
+  notFoundMessages: {
     en: {
       h1: 'Oops!',
       h2: 'Page is not found',
@@ -106,6 +106,26 @@ const messages = {
       h2: 'Page non retrouvée',
       details: 'Désolé, une erreur est survenue ou la page demandée est introuvable.',
       actions: 'Page d\'accueil'
+    }
+  },
+  tour: {
+    en: {
+      map: '"Interact with the map. You can click on markers and see each city weather cards. Right click on two markers to reveal a directions link. Also drag days control to see how tempreture will be in the next days."',
+      cards: '"You can drag cards to the weather comparisons area. Colored cards reflect maximum and minimum temperature in one day. Click on min-max to simplify comparisions visually."',
+      comparision: '"In comparison area You can see cards of different days and of different cities too. For Android devices and on Google Chrome only, You can also share any card with people."',
+      gallery: '"A beautiful local gallery of pictures of the main city."'
+    },
+    ar: {
+      map: '"تفاعل مع الخريطة. يمكنك النقر فوق العلامات ورؤية بطاقات الطقس الخاصة بكل مدينة. انقر بزر الماوس الأيمن على علامتين للكشف عن رابط الاتجاهات. اسحب أيضًا زر الأيام لترى كيف تقلبات درجة الحرارة في الأيام القادمة."',
+      cards: '"يمكنك سحب البطاقات إلى منطقة مقارنة الطقس. تعكس البطاقات الملونة درجة الحرارة القصوى والدنيا في يوم واحد. انقر فوق min-max لتبسيط المقارنات بصريًا."',
+      comparision: '"في منطقة المقارنة ، يمكنك رؤية بطاقات أيام مختلفة ومدن مختلفة أيضًا. بالنسبة لأجهزة Android وعلى Google Chrome فقط ، يمكنك أيضًا مشاركة أي بطاقة مع الأشخاص."',
+      gallery: '"عرض جميل لصور المدينة الرئيسية."'
+    },
+    fr: {
+      map: '"Interagissez avec la carte. Vous pouvez cliquer sur les marqueurs et voir les cartes météo de chaque ville. Faites un clic droit sur deux marqueurs pour révéler un lien de directions. Faites également glisser le «controleur des jours» pour voir comment la température sera dans les prochains jours. "',
+      cards: '"Vous pouvez glisser des cartes vers la zone de comparaison. Les cartes colorées reflètent la température maximale et minimale en une journée. Cliquez sur min-max pour simplifier visuellement les comparaisons."',
+      comparision: '"Dans la zone de comparaison Vous pouvez voir des cartes de différents jours et de différentes villes aussi. Pour les appareils Android et sur Google Chrome uniquement, vous pouvez également partager n importe quelle carte avec d autres personnes."',
+      gallery: '"Une belle galerie locale de photos de la ville principale."'
     }
   }
 }
